af1b2dfb70c502f1089d06924ccf427ed2cbfb8b
pphal <81phary@gmail.com>
2023-06-27 16:13:55 +1000

    adding test methods clickDownUp

--- src/test/java/com/accesshq/selenium/IntroSelenium.java
+++ src/test/java/com/accesshq/selenium/IntroSelenium.java
@@ -13,21 +13,41 @@ import static org.hamcrest.Matchers.is;
 
 public class IntroSelenium {
     private final String url = "https://d18u5zoaatmpxx.cloudfront.net/#/";
-   @Test
-    public void chromeBtnClickedTest(){
-       //Method 1 (old method -- might encounter security issues)
-//       WebDriver chromeDriver = new ChromeDriver();
-//       chromeDriver.get(url);
-//       chromeDriver.findElement(By.id("forename")).sendKeys("Megatron");
-//       chromeDriver.findElement(By.id("submit")).click();
 
-       //Method 2 (new method)
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("--allow-all-origin=*");
-       WebDriver chromeDriver = new ChromeDriver(options);
-       chromeDriver.get(url);
-       chromeDriver.findElement(By.id("forename")).sendKeys("Megatron");
-       chromeDriver.findElement(By.id("submit")).click();
-   }
+//   @Test
+//    public void chromeBtnClickedTest(){
+//       //Method 1 (old method -- might encounter security issues)
+////       WebDriver chromeDriver = new ChromeDriver();
+////       chromeDriver.get(url);
+////       chromeDriver.findElement(By.id("forename")).sendKeys("Megatron");
+////       chromeDriver.findElement(By.id("submit")).click();
+//
+//       //Method 2 (new method)
+//       ChromeOptions options = new ChromeOptions();
+//       options.addArguments("--allow-all-origin=*");
+//       WebDriver chromeDriver = new ChromeDriver(options);
+//       chromeDriver.get(url);
+//       chromeDriver.findElement(By.id("forename")).sendKeys("Optimus Prime");
+//       chromeDriver.findElement(By.id("submit")).click();
+//   }
+
+//    @Test
+//    public void chromeLoginBtnClickedTest(){
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--allow-all-origin=*");
+//        WebDriver chromeDriver = new ChromeDriver(options);
+//        chromeDriver.get(url);
+//        chromeDriver.findElement(By.id("loginButton")).click();
+//    }
+
+    @Test
+    public void chromeClickedDownUpLinksTest(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--allow-all-origin=*");
+        WebDriver chromeDriver = new ChromeDriver(options);
+        chromeDriver.get(url);
+        chromeDriver.findElement(By.className("anibtn")).click();
+        chromeDriver.findElement(By.className("transition-y")).click();
+    }
 
 }
